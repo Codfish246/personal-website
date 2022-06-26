e7604dd14f97b98112d928431f6c7f3b46fd82ca
Luca <codfish@stuff.solutions>
2022-06-26 11:28:07 +0100

    add html bio age updater age.js

---
+++ assets/age.js
@@ -0,0 +1,8 @@
+//little bit of hardcoded js for calculating age and updating bio in html - @codfish246
+window.onload = function () {
+  var today = new Date();
+  var date = new Date(2003, 01, 21, 0, 0, 1);
+  var difference = (today - date) / (1000 * 60 * 60 * 24 * 365);
+  var dToDisplay = difference.toFixed(0);
+  document.getElementById("age").innerHTML = dToDisplay;
+};
