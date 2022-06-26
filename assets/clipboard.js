8c8cd9b4ca863e78eaeccdaaff1315fe403cf865
Luca <codfish@stuff.solutions>
2022-06-26 11:28:37 +0100

    add discord user copier thing clipboard.js

---
+++ assets/clipboard.js
@@ -0,0 +1,14 @@
+//little bit of hardcoded js for copying something predefined to user's clipboard on button click - @codfish246
+window.onload = function () {
+  document.getElementById("discordcopy").onclick = function () {
+    let text = "Codfish246#0075";
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert("Codfish246#0075 copied to clipboard");
+      })
+      .catch((err) => {
+        alert("Error copying text: ", err);
+      });
+  };
+};
