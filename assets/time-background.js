75a7c3f9c13f9bd7db63887f82c7bddc5b42e248
Luca <codfish@stuff.solutions>
2022-07-04 05:39:28 +0100

    change jpgs to smaller webps & gitignore

--- assets/time-background.js
+++ assets/time-background.js
@@ -3,8 +3,8 @@ window.onload = function () {
   var n = d.getHours();
   console.log("hour: " + n);
   if (n > 21 || n < 6)
-    $("body").css("background-image", "url('assets/night.jpg')");
+    $("body").css("background-image", "url('assets/night-small.webp')");
   else if (n > 15 && n < 21)
-    $("body").css("background-image", "url('assets/sunset.jpg')");
-  else $("body").css("background-image", "url('assets/day.jpg')");
+    $("body").css("background-image", "url('assets/sunset.webp')");
+  else $("body").css("background-image", "url('assets/day-small.webp')");
 };
