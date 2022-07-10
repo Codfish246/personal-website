b09d4483c23f6d3c8fb5aa06927197751c66acd6
Luca <codfish@stuff.solutions>
2022-07-10 08:47:54 +0100

    add sunrise background js and change times

--- assets/time-background.js
+++ assets/time-background.js
@@ -2,9 +2,11 @@ window.onload = function () {
   var d = new Date();
   var n = d.getHours();
   console.log("hour: " + n);
-  if (n > 21 || n < 6)
+  if (n >= 21 || n < 0)
     $("body").css("background-image", "url('assets/night-small.webp')");
-  else if (n > 15 && n < 21)
+  else if (n >= 5 && n <= 9)
+    $("body").css("background-image", "url('assets/sunrise.webp')");
+  else if (n > 19 && n < 21)
     $("body").css("background-image", "url('assets/sunset.webp')");
   else $("body").css("background-image", "url('assets/day-small.webp')");
 };
