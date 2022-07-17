faebc0897b6df69ffa4c865d08d16084d8b494e7
Luca <codfish@stuff.solutions>
2022-07-17 05:00:24 +0100

    update js reactive background timings

--- assets/time-background.js
+++ assets/time-background.js
@@ -2,11 +2,13 @@ window.onload = function () {
   var d = new Date();
   var n = d.getHours();
   console.log("hour: " + n);
-  if (n >= 21 || n < 0)
+  if (n >= 21 || n < 1)
     $("body").css("background-image", "url('assets/night-small.webp')");
   else if (n >= 5 && n <= 9)
     $("body").css("background-image", "url('assets/sunrise.webp')");
   else if (n > 19 && n < 21)
     $("body").css("background-image", "url('assets/sunset.webp')");
-  else $("body").css("background-image", "url('assets/day-small.webp')");
+  else if (n >= 10 && n <= 19)
+    $("body").css("background-image", "url('assets/day-small.webp')");
+  else console.log("non-image pitch-black sky background set");
 };
