dbc36a00a3ac0912bb3bafcf821665686df65719
Luca <codfish@stuff.solutions>
2022-08-21 22:15:32 +0100

    fix autoupdating age js

--- assets/utils.js
+++ assets/utils.js
@@ -3,10 +3,11 @@
 //little bit of js for calculating age and updating bio in html
 function updateAge() {
   var today = new Date();
-  var date = new Date(2003, 01, 21, 0, 0, 1);
-  var difference = (today - date) / (1000 * 60 * 60 * 24 * 365);
-  var dToDisplay = difference.toFixed(0);
-  document.getElementById("age").innerHTML = dToDisplay;
+  var date = new Date(2003, 01, 26, 0, 0, 1);
+  var difference = today - date;
+  var age = Math.floor(difference / 31536000000);
+  //var dToDisplay = difference.toFixed(0);
+  document.getElementById("age").innerHTML = age;
 }
 
 // toggles settings menu on click
